f52a323d138dd3fcec634576669297a89d04e769
Avadheshkumarchauhan <avadheaha@gmail.com>
2025-08-16 18:54:46 +0530

    add this file in repo

---
+++ opps/basic.js
@@ -0,0 +1,16 @@
+ class Product{
+    //properties -> variables --> data member
+    name ;
+    price;
+    rating;
+    //behaviours --> function --> member functions
+    //Create function without keyword
+    dislay(){
+        console.log("Displaying behaviours ! ");
+        
+    }
+ }
+ const p=new Product();
+ console.log(p);
+ p.dislay()
+ 
