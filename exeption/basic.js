f1c320d69e0a3dc464495e4623341c35f0f38076
Avadheshkumarchauhan <avadheaha@gmail.com>
2025-08-16 17:40:09 +0530

    add this file in repo

---
+++ exeption/basic.js
@@ -0,0 +1,13 @@
+try{
+x=undefined;
+console.log(x[0]);
+
+}
+catch(e){
+    console.log("Error ",e);
+    
+}
+finally{
+    console.log("End of code ");
+    
+}
---
+++ exeption/basic1.js
@@ -0,0 +1,16 @@
+try{
+    console.log("Hello Avadhesh ");
+    console.log(a);
+    console.log("How are you ");  
+    
+}
+catch(err){
+    console.log("Error in try code and error type is ",err);
+    
+}
+
+finally{
+    console.log("End of code this line and finnaly statement ");
+    
+}
+console.log("End !!!!!!");
